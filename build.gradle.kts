b7fc8375589b1955d942184cb6c6c474c34031d3
Thomson Thomas <thomsontomy@gmail.com>
2021-12-05 22:50:58 +1100

    Convert to kts

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        gradlePluginPortal()
+        jcenter()
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31")
+        classpath("com.android.tools.build:gradle:7.0.3")
+    }
+}
+
+group = "me.thomson"
+version = "1.0"
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+/*task clean(type: Delete) {
+    delete rootProject.buildDir
+}*/
---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        jcenter()
+        gradlePluginPortal()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+
+}
+rootProject.name = "KotlinKidspaint"
+
+
+include(":app")
+//include(":desktop")
+//include(":common")
